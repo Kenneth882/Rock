9f2f5d975dd67a42b7d5e0a24398b12a8ae632c0
Kenneth Romero <romerokenny882@gmail.com>
2024-08-15 16:36:33 -0400

    added index and script files as well as completing the computer part of rock paper sciisors

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function getComputerChoice(){
+return Math.floor(Math.random()*3);
+}
+
+const a=getComputerChoice();
+const b=getComputerChoice();
+const c=getComputerChoice();
+
+console.log(a);
+console.log(b);
+console.log(c);
